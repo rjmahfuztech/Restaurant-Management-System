e12da60a19c520ee2a3ce75c3ab10bddbc854f04
rjmahfuztech <rjmahfuztech@gmail.com>
2024-12-11 20:22:53 +0600

    user,employee,admin class added

--- user.py
+++ user.py
@@ -14,12 +14,24 @@ class Employee(User):
         self.designation = designation
         self.salary = salary
 
-emp = Employee('mahfuz','mah@gmail.com',39493,'Baya,Rajshahi',22,'Student',1000)
+class Admin(User):
+    def __init__(self,name,email,phone,address):
+        super().__init__(name,email,phone,address)
+        self.employees = []
 
-print(emp.address)
-# class Admin(User):
-#     def __init__(self,name,email,phone,address):
-#         super().__init__(name,email,phone,address)
-#         self.employees = []
+    def add_employee(self,name,email,phone,address,age,designation,salary):
+        employee = Employee(name,email,phone,address,age,designation,salary)
+        self.employees.append(employee)
+        print(f'Added an employee, name = {name}')
 
-#     def add_employee()
+    def view_employee(self):
+        for emp in self.employees:
+            print(emp.name,emp.age,emp.address,emp.email)
+
+
+user = Admin('Mahfuz','mahfuz@gmail.com',2044839,'Rajshahi')
+user.add_employee('rohim','rohim@gmail.com',99548,'Tanor',19,'player',50002)
+user.add_employee('sakib','rohim@gmail.com',99548,'Tanor',15,'player',50002)
+user.add_employee('rakib','rohim@gmail.com',99548,'Tanor',23,'player',50002)
+
+user.view_employee()
